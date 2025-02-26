958b41ae8bb82d76f27cd89966cdbf73091e1871
ekaardilahfebriyanti <ardilaheka@gmail.com>
2025-02-26 17:03:59 +0700

    memperbaiki responsivitas dihalaman utama tiap kategori

--- resources/views/football/index.blade.php
+++ resources/views/football/index.blade.php
@@ -7,24 +7,24 @@
         <div class="row">
             <div class="col-12">
                 <div class="row">
-                    <div class="col-8">
-                        <p>hot articles</p>
+                    <div class="col-lg-8 col-md-8 col-sm-6">
+                        <p class="responsive-text">HOT ARTICLES</p>
                     </div>
-                    <div class="col-4">
-                        <p>EXPLORE NEWS ABOUT football ON LUDUS. YOU'LL GET THE LATEST INFORMATION HERE.</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p>About Football News</p>
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <p class="fs-5 text-muted mt-3">EXPLORE NEWS ABOUT football ON LUDUS. YOU'LL GET THE LATEST INFORMATION HERE.</p>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-4">
-                        <p>From the latest match highlights to immersive stadium experiences and fan events.</p>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <p class="responsive-text text-uppercase">About Football News</p>
                     </div>
-                    <div class="col-8">
-                        <p>might you like</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
+                        <p class="fs-5 text-uppercase text-muted">From the latest match highlights to immersive stadium experiences and fan events.</p>
+                    </div>
+                    <div class="col-lg-8 col-md-8 col-sm-6">
+                        <p class="responsive-text text-uppercase">might you like</p>
                     </div>
                 </div>
             </div>
@@ -34,15 +34,14 @@
 <!-- Lates News -->
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand"><h3>Lates News</h3></a>
-    <a href="{{ route('football.viewall') }}" type="button" class="btn btn-outline-dark bt1">View All</a>
-    <form class="d-flex" role="search" method="GET" action="{{ route('football.index') }}">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search" value="{{ request()->input('search') }}">
-      <button class="btn btn-outline-success" type="submit">
-        <svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true" class="DocSearch-Search-Icon"><path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path>
-        </svg>
-      </button>
-    </form>
+    <div class="row w-100">
+    <div class="col-3 col-md-2 col-sm-9">
+            <a class="navbar-brand"><h5>Lates News</h5></a>
+        </div>
+    </div>
+    <div class="col-3 col-md-2 col-sm-3">
+        <a href="{{ route('football.viewall') }}" type="button" class="btn btn-outline-dark p-1">View All</a>
+    </div>
   </div>
 </nav>
 <!-- Data artikel terbaru -->
@@ -52,7 +51,7 @@
             <p>Data Kosong</p>
         @else
             @foreach ($articles as $article)
-            <div class="col mt-3">
+            <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
                 <div class="card">
                         @if($article->type=='series')
                         <a href="{{route('football.series', $article->id)}}">
@@ -89,7 +88,7 @@
             </div>
             @endforeach
         @endif
-        <div class="col mt-3">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title title1">Subscribe so you don't miss out!</h5>
@@ -108,14 +107,20 @@
 <!-- Hightlight -->
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand"><h3>Highlights</h3></a>
-    <a href="{{ route('football.viewall') }}" type="button" class="btn btn-outline-dark bt3">View All</a>
+    <div class="row w-100 mt-4">
+        <div class="col-3 col-md-2 col-sm-9">
+            <a class="navbar-brand"><h5>Highlights</h5></a>
+        </div>
+        <div class="col-3 col-md-2 col-sm-3">
+            <a href="{{ route('football.viewall') }}" type="button" class="btn btn-outline-dark p-1">View All</a>
+        </div>
+    </div>
   </div>
 </nav>
 <!-- Data hightlight artikel yang sedang tranding -->
 <div class="container-fluid pg-3">
     <div class="row">
-        <div class="col-7">
+        <div class="col-lg-7 col-md-7 col-sm-6">
             @if($highlightPost==null)
                 <p>Data Kosong</p>
             @else 
@@ -131,18 +136,18 @@
                     <path d="M7.5 11.6667C7.73611 11.6667 7.93417 11.5867 8.09417 11.4267C8.25417 11.2667 8.33389 11.0689 8.33333 10.8333C8.33278 10.5978 8.25278 10.4 8.09333 10.24C7.93389 10.08 7.73611 10 7.5 10C7.26389 10 7.06611 10.08 6.90667 10.24C6.74722 10.4 6.66722 10.5978 6.66667 10.8333C6.66611 11.0689 6.74611 11.2669 6.90667 11.4275C7.06722 11.5881 7.265 11.6678 7.5 11.6667ZM6.66667 8.33333H8.33333V3.33333H6.66667V8.33333ZM4.375 15L0 10.625V4.375L4.375 0H10.625L15 4.375V10.625L10.625 15H4.375ZM5.08333 13.3333H9.91667L13.3333 9.91667V5.08333L9.91667 1.66667H5.08333L1.66667 5.08333V9.91667L5.08333 13.3333Z" fill="#060606"/></svg>
                 </a>
             </div>
-            <img src="{{ asset('images_download/'.$highlightPost->image1) }}" alt="{{$highlightPost->headlineUtamaArtikel}}">
-            <div class="caption">
+            <img class="w-100" src="{{ asset('images_download/'.$highlightPost->image1) }}" alt="{{$highlightPost->headlineUtamaArtikel}}">
+            <div class="caption" style="margin-top: -25%; margin-left: 5%;">
                 @if($highlightPost->type=='series')
-                <h1 class="card-title"><a class="text-decoration-none text-white" href="{{route('football.series', $highlightPost->id)}}">{{$highlightPost->headlineUtamaArtikel}}</a></h1>
+                <h1 class="card-title"><a class="text-decoration-none text-black" href="{{route('football.series', $highlightPost->id)}}">{{$highlightPost->headlineUtamaArtikel}}</a></h1>
                 @else
-                <h1 class="card-title"><a class="text-decoration-none text-white" href="{{route('football.show', $highlightPost->id)}}">{{$highlightPost->headlineUtamaArtikel}}</a></h1>
+                <h1 class="card-title"><a class="text-decoration-none text-black" href="{{route('football.show', $highlightPost->id)}}">{{$highlightPost->headlineUtamaArtikel}}</a></h1>
                 @endif
                 <p>{{ \Carbon\Carbon::parse($highlightPost->created_at)->format('d M Y') }}</p>
             </div>
             @endif
         </div>
-        <div class="col-5">
+        <div class="col-lg-5 col-md-5 col-sm-6 mt-4">
             @if($sideHighlight==null)
                 <p>Data Kosong</p>
             @else 
@@ -181,7 +186,7 @@
             <p>Data Kosong</p>
         @else 
             @foreach($trendingPosts as $trending)
-            <div class="col-4">
+            <div class="col-lg-4 col-md-4 col-sm-12">
                 <div class="card">
                     <img src="{{ asset('images_download/'.$trending->image1) }}" class="card-img-top" alt="{{$trending->headlineUtamaArtikel}}">
                     <div class="card-body">
@@ -211,17 +216,23 @@
 <!-- Recommendation -->
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand"><h3>Recomendation For You</h3></a>
-    <a href="{{ route('football.viewall') }}" type="button" class="btn btn-outline-dark bt2">View All</a>
+    <div class="row">
+        <div class="col-8 col-md-8 col-sm-9">
+            <a class="navbar-brand"><h5>Recomendation For You</h5></a>
+        </div>
+        <div class="col-4 col-md-4 col-sm-3">
+            <a href="{{ route('football.viewall') }}" type="button" class="btn btn-outline-dark p-1">View All</a>
+        </div>
+    </div>
   </div>
 </nav>
 <!-- artikel trending lainnya dan iklan -->
 <div class="container-fluid pg-5">
     <div class="row">
         <!-- iklan  -->
-        <div class="col-4">
-            <div class="card1">
-                <p>Ingin Mengajukan Iklan di Artikel Ini?
+        <div class="col-lg-4 col-md-4 col-sm-12">
+            <div class="d-inline-block bg-salmon p-5">
+                <p class="fs-1">Ingin Mengajukan Iklan di Artikel Ini?
                 Klik Sekarang Dan Dapatkan Harga Terbaik</p>
                 <button type="button" class="btn btn-outline-dark btn-n1" data-bs-toggle="modal" data-bs-target="#advertisementModal">
                 Request Advertorial
@@ -236,7 +247,7 @@
             <p>Data Kosong</p>
         @else 
             @foreach($recommendations as $rec)
-                <div class="col-4">
+                <div class="col-lg-4 col-md-4 col-sm-12">
                     <div class="card">
                         <img src="{{ asset('images_download/'.$rec->image1) }}" class="card-img-top" alt="{{$rec->headlineUtamaArtikel}}">
                         <div class="card-body">
